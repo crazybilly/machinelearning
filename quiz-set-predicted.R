f890fb2e94865f4977bd3593107edbd25ba3b233
crazybilly <crazybilly@gmail.com>
2015-01-25 01:23:54 -0600

    add script for quiz results

--- quiz-set-predicted.R
+++ quiz-set-predicted.R
@@ -15,3 +15,15 @@ if( !exists('modelt5')) {
 # quiz results ------------------------------------------------------------
 
 qpredict  <- predict(modelt5,quiz)
+
+
+
+# function ----------------------------------------------------------------
+
+pml_write_files = function(x){
+  n = length(x)
+  for(i in 1:n){
+    filename = paste0("problem_id_",i,".txt")
+    write.table(x[i],file=filename,quote=FALSE,row.names=FALSE,col.names=FALSE)
+  }
+}
